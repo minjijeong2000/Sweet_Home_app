75fb4cf135a54b834f3544059b44c422a486e58f
Minji <dudvnd13@gmail.com>
2023-05-09 13:00:43 -0700

    Finalize app

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter_app_demo/user_profile.dart';
 import 'package:flutter_app_demo/next_page.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dart:async';
 
 
@@ -18,7 +19,8 @@ Future<void> main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final Stream<QuerySnapshot> users = FirebaseFirestore.instance.collection('users').snapshots();
+  MyApp({super.key});
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
